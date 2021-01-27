b2a98d1a47312c2fcd5d8bf682f41a9641ca307c
Filip FICO <fitz993m@gmail.com>
2021-01-27 21:01:47 +0100

    1.6.  Server config

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-const port = process.env.PORT || 4000;
+const bodyParser = require('body-parser');
 
-app.listen(port, () => `Listening on port ${port}`);
+mongoose
+  .connect(`mongodb://localhost/${process.env.DB_NAME}`, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log(`Connected to MongoDB`))
+  .catch((err) => console.log('Could not connect to MongoDB'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Request-With, Authorization, Accept, Content-Type'
+  );
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+const port = process.env.DB_PORT || 4000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
