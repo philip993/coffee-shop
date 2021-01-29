6d26d29d0c1110213ba70928f44b4e7ed936f74a
Filip Mihajlovski <fitz993m@gmail.com>
2021-01-29 16:49:29 +0100

    1.20. Icons added

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -3,6 +3,9 @@ import './HomepageStyle.scss';
 import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import MenuIcon from '../../img/menu.svg';
+import OrderIcon from '../../img/order.svg';
+import ProfileIcon from '../../img/profile.svg';
+import ContactIcon from '../../img/contact.svg';
 
 const Homepage = () => {
   return (
@@ -17,24 +20,21 @@ const Homepage = () => {
         <h2>Don't wait in line, make your order now!</h2>
       </div>
       <div className="homepageSecondDiv">
-        <div className="link">
-          <img src={MenuIcon} alt="menu" />
-          <Link>Check Menu</Link>
+        <div className="linkContainer">
+          <img className="icons" src={MenuIcon} alt="menu" />
+          <Link className="link">Check Menu</Link>
         </div>
-        <div className="link">
-          {' '}
-          Orders
-          <Link>Show Orders</Link>
+        <div className="linkContainer">
+          <img className="icons" src={OrderIcon} alt="order" />
+          <Link className="link">Show Orders</Link>
         </div>
-        <div className="link">
-          {' '}
-          Profile
-          <Link>View Profile</Link>
+        <div className="linkContainer">
+          <img className="icons" src={ProfileIcon} alt="profile" />
+          <Link className="link">View Profile</Link>
         </div>
-        <div className="link">
-          {' '}
-          Contact
-          <Link>Contact Us</Link>
+        <div className="linkContainer">
+          <img className="icons" src={ContactIcon} alt="contact" />
+          <Link className="link">Contact Us</Link>
         </div>
       </div>
     </div>
