619ca748186a48d7d0fba6023e2e7b67f83f9f8d
Filip Mihajlovski <fitz993m@gmail.com>
2021-02-04 15:45:06 +0100

    2.6. Menu layout

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,36 +1,44 @@
 import React from 'react';
 import './MenuStyle.scss';
-import { Card, CardContent, Select, MenuItem, Button } from '@material-ui/core';
+import { Card, CardContent, Button } from '@material-ui/core';
 import EspressoImg from '../../img/espresso.jpg';
+import BeanStar from '../../img/bean-stars.svg';
+import WaterStar from '../../img/water-stars.svg';
+import CupStar from '../../img/cup-stars.svg';
 
 const Menu = () => {
   return (
     <div className="menuDiv">
       <div className="menuFirstDiv">
-        <h1>Menu</h1>
+        <h1>Coffe Shop Menu</h1>
       </div>
       <div className="menuSecondDiv">
         <Card className="card">
-          <h4 className="cardHeader">Espresso</h4>
+          <CardContent className="cardHeader">
+            <h4>Espresso</h4>
+            <h4 className="price">$ 2.00</h4>
+          </CardContent>
           <img className="cardMedia" src={EspressoImg} alt="espresso" />
           <CardContent className="cardContent">
-            Beans{' '}
-            <Select>
-              <MenuItem>Arabica</MenuItem>
-              <MenuItem>Robusta</MenuItem>
-            </Select>
-            Sugar{' '}
-            <Select>
-              <MenuItem>No sugar</MenuItem>
-              <MenuItem>1/2 tbps</MenuItem>
-              <MenuItem>1 tbps</MenuItem>
-              <MenuItem>1 1/2 tbps</MenuItem>
-              <MenuItem>2 tbps</MenuItem>
-            </Select>
+            <span className="detailSpan">
+              <p>Coffee Details</p>
+            </span>
+            <span className="starSpan">
+              <p>Intensity</p>
+              <img className="beanStar" src={BeanStar} alt="star" />{' '}
+              <img className="beanStar" src={BeanStar} alt="star" />
+            </span>
+            <span className="starSpan">
+              <p>Water</p>
+              <img className="beanStar" src={WaterStar} alt="star" />{' '}
+            </span>
+            <span className="starSpan">
+              <p>Size</p>
+              <img className="beanStar" src={CupStar} alt="star" />{' '}
+            </span>
           </CardContent>
           <CardContent className="cardFooter">
-            <Button>- 1</Button>
-            <Button>+ 1</Button>
+            <Button className="btnPlus">ADD</Button>
           </CardContent>
         </Card>
       </div>
