9b44a220e594fb770b8524c2145e3a4c33d68441
Filip Mihajlovski <fitz993m@gmail.com>
2021-02-19 14:39:42 +0100

    3.3. Profile content

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import './ProfileStyle.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Navbar from '../Navbar/Navbar';
+import { Avatar } from '@material-ui/core';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Profile = () => {
   return (
     <div className="profile">
-      <h1>Profile Page</h1>
+      <Navbar />
+      <div className="profileFirstDiv">
+        <h1>Profile Page</h1>
+      </div>
+      <div className="profileSecondDiv">
+        <div className="card">
+          <div className="personal">
+            <Avatar className="avatar">JD</Avatar>
+            <h1>John Doe</h1>
+            <span className="location">
+              <FontAwesomeIcon className="locationIcon" icon={faMapMarkerAlt} />{' '}
+              <h4>New York</h4>
+            </span>
+          </div>
+          <div className="member">
+            <h4>Gold Member</h4>
+            <h5>2021</h5>
+          </div>
+        </div>
+        <div className="content">
+          <h1>Content</h1>
+        </div>
+      </div>
     </div>
   );
 };
