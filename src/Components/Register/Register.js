85d3879293ec1c7d5548dd70875591525cefe6be
Filip Mihajlovski <fitz993m@gmail.com>
2021-03-05 16:01:15 +0100

    6.3. Register component

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import './RegisterStyle.scss';
+import { FormGroup, FormLabel, InputBase, Button } from '@material-ui/core';
 
 const Register = () => {
   return (
     <div className="register">
-      <h1>Register</h1>
+      <div className="contactForm">
+        <h1>Register</h1>
+        <form className="form">
+          <FormGroup className="formGroup">
+            <FormLabel className="formLabel">
+              <p>Full Name *</p>
+            </FormLabel>
+            <InputBase className="formInput" placeholder="e.g. John Doe" />
+          </FormGroup>
+          <FormGroup className="formGroup">
+            <FormLabel className="formLabel">
+              <p>Email *</p>
+            </FormLabel>
+            <InputBase className="formInput" placeholder="e.g. john@john.com" />
+          </FormGroup>
+          <FormGroup className="formGroup">
+            <FormLabel className="formLabel">
+              <p>Password *</p>
+            </FormLabel>
+            <InputBase
+              className="formInput"
+              placeholder="Must be at least 4 characters.."
+            />
+          </FormGroup>
+          <FormGroup className="formGroup">
+            <FormLabel className="formLabel">
+              <p>Phone *</p>
+            </FormLabel>
+            <InputBase className="formInput" placeholder="e.g. +100 555 1111" />
+          </FormGroup>
+          <FormGroup className="formGroup">
+            <Button className="btn">Register</Button>
+          </FormGroup>
+        </form>
+      </div>
     </div>
   );
 };
