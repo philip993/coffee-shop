45250f6f017c50135a3c28420bf451f09fe19d2f
Filip Mihajlovski <fitz993m@gmail.com>
2021-03-08 15:41:52 +0100

    7.3. Login component

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import './Login.scss';
+import { FormGroup, FormLabel, InputBase } from '@material-ui/core';
 
 const Login = () => {
   return (
     <div className="login">
-      <h1>Login</h1>
+      <div className="contactForm">
+        <h1>Login</h1>
+        <form className="form">
+          <FormGroup className="formGroup">
+            <FormLabel className="formLabel">
+              <p>Email *</p>
+            </FormLabel>
+            <InputBase className="formInput" placeholder="e.g. john@john.com" />
+          </FormGroup>
+          <FormGroup className="formGroup">
+            <FormLabel className="formLabel">
+              <p>Password *</p>
+            </FormLabel>
+            <InputBase
+              className="formInput"
+              placeholder="Enter a valid password...."
+            />
+          </FormGroup>
+        </form>
+      </div>
     </div>
   );
 };
