3cdb495e9e97b442639a948fc85f6e7929058bd3
Filip Mihajlovski <fitz993m@gmail.com>
2021-03-10 15:20:01 +0100

    8.6. Root reducer updated for Login & Register

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import { MainReducer } from '../Components/Main/MainReducer';
+import { RegisterReducer } from '../Components/Register/RegisterReducer';
+import { LoginReducer } from '../Components/Login/LoginReducer';
 
 const rootReducer = combineReducers({
   MainReducer,
+  RegisterReducer,
+  LoginReducer,
 });
 
 export default rootReducer;
