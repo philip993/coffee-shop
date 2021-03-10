c8b69f0851da6dcb760d86d86e4e87da3cd0c116
Filip Mihajlovski <fitz993m@gmail.com>
2021-03-10 14:50:21 +0100

    7.14. Added email & password input

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './Login.scss';
 import { FormGroup, FormLabel, InputBase, Button } from '@material-ui/core';
+import { useSelector, useDispatch } from 'react-redux';
+import { inputLoginPassword, inputLoginEmail } from './LoginActions';
 
 const Login = () => {
+  const { email, password } = useSelector((state) => ({
+    ...state.LoginReducer,
+  }));
+  const dispatch = useDispatch();
+
+  const handleInputEmail = (e) => {
+    dispatch(inputLoginEmail(e.target.value));
+  };
+
+  const handleInputPassword = (e) => {
+    dispatch(inputLoginPassword(e.target.value));
+  };
   return (
     <div className="login">
       <div className="contactForm">
@@ -12,7 +26,12 @@ const Login = () => {
             <FormLabel className="formLabel">
               <p>Email *</p>
             </FormLabel>
-            <InputBase className="formInput" placeholder="e.g. john@john.com" />
+            <InputBase
+              className="formInput"
+              value={email}
+              onChange={handleInputEmail}
+              placeholder="e.g. john@john.com"
+            />
           </FormGroup>
           <FormGroup className="formGroup">
             <FormLabel className="formLabel">
@@ -20,6 +39,8 @@ const Login = () => {
             </FormLabel>
             <InputBase
               className="formInput"
+              value={password}
+              onChange={handleInputPassword}
               placeholder="Enter a valid password...."
             />
           </FormGroup>
