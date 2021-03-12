f1b28c817c4a220c439cfa8a9de33b0d989adca0
Filip Mihajlovski <fitz993m@gmail.com>
2021-03-12 16:23:10 +0100

    8.7. Action and action types

--- src/Components/Register/RegisterActions.js
+++ src/Components/Register/RegisterActions.js
@@ -1,7 +1,32 @@
-import { SET_REGISTER_NEXT_PAGE } from './RegisterActionTypes';
+import {
+  SET_REGISTER_NEXT_PAGE,
+  SET_REGISTER_PREVIOUS_PAGE,
+  INPUT_REGISTER_FULL_NAME,
+} from './RegisterActionTypes';
+import { INPUT_LOGIN_EMAIL } from '../Login/LoginActionTypes';
 
 export const setRegisterFirstPage = () => {
   return {
     type: SET_REGISTER_NEXT_PAGE,
+  };
+};
+
+export const setRegisterPreviousPage = () => {
+  return {
+    type: SET_REGISTER_PREVIOUS_PAGE,
+  };
+};
+
+export const inputRegisterFullname = (e) => {
+  return {
+    type: INPUT_REGISTER_FULL_NAME,
+    payload: e,
+  };
+};
+
+export const inputRegisterEmail = (e) => {
+  return {
+    type: INPUT_LOGIN_EMAIL,
+    payload: e,
   };
 };
