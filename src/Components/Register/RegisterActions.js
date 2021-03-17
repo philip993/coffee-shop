9d058ee166323c398328716e9c2f81bd6b0d9b3f
Filip Mihajlovski <fitz993m@gmail.com>
2021-03-17 16:02:34 +0100

    8.8. Actions completed

--- src/Components/Register/RegisterActions.js
+++ src/Components/Register/RegisterActions.js
@@ -2,8 +2,14 @@ import {
   SET_REGISTER_NEXT_PAGE,
   SET_REGISTER_PREVIOUS_PAGE,
   INPUT_REGISTER_FULL_NAME,
+  INPUT_REGISTER_PASSWORD,
+  INPUT_REGISTER_REPEAT_PASSWORD,
+  INPUT_REGISTER_ADDRESS,
+  INPUT_REGISTER_CITY,
+  INPUT_REGISTER_CARD,
+  INPUT_REGISTER_PHONE,
+  INPUT_REGISTER_EMAIL,
 } from './RegisterActionTypes';
-import { INPUT_LOGIN_EMAIL } from '../Login/LoginActionTypes';
 
 export const setRegisterFirstPage = () => {
   return {
@@ -26,7 +32,49 @@ export const inputRegisterFullname = (e) => {
 
 export const inputRegisterEmail = (e) => {
   return {
-    type: INPUT_LOGIN_EMAIL,
+    type: INPUT_REGISTER_EMAIL,
+    payload: e,
+  };
+};
+
+export const inputRegisterPassword = (e) => {
+  return {
+    type: INPUT_REGISTER_PASSWORD,
+    payload: e,
+  };
+};
+
+export const inputRegisterRepeatPassword = (e) => {
+  return {
+    type: INPUT_REGISTER_REPEAT_PASSWORD,
+    payload: e,
+  };
+};
+
+export const inputRegisterAddress = (e) => {
+  return {
+    type: INPUT_REGISTER_ADDRESS,
+    payload: e,
+  };
+};
+
+export const inputRegisterCity = (e) => {
+  return {
+    type: INPUT_REGISTER_CITY,
+    payload: e,
+  };
+};
+
+export const inputRegisterCard = (e) => {
+  return {
+    type: INPUT_REGISTER_CARD,
+    payload: e,
+  };
+};
+
+export const inputRegisterPhone = (e) => {
+  return {
+    type: INPUT_REGISTER_PHONE,
     payload: e,
   };
 };
