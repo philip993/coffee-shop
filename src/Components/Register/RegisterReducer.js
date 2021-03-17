e9273d57bcde4e815ab8149a23b60ffab12085e4
Filip Mihajlovski <fitz993m@gmail.com>
2021-03-17 16:02:50 +0100

    8.9. Reducer completed

--- src/Components/Register/RegisterReducer.js
+++ src/Components/Register/RegisterReducer.js
@@ -1,7 +1,26 @@
-import { SET_REGISTER_NEXT_PAGE } from './RegisterActionTypes';
+import {
+  SET_REGISTER_NEXT_PAGE,
+  SET_REGISTER_PREVIOUS_PAGE,
+  INPUT_REGISTER_FULL_NAME,
+  INPUT_REGISTER_EMAIL,
+  INPUT_REGISTER_PASSWORD,
+  INPUT_REGISTER_REPEAT_PASSWORD,
+  INPUT_REGISTER_ADDRESS,
+  INPUT_REGISTER_CITY,
+  INPUT_REGISTER_CARD,
+  INPUT_REGISTER_PHONE,
+} from './RegisterActionTypes';
 
 const initialState = {
   page: '',
+  regFullName: '',
+  regEmail: '',
+  regPassword: '',
+  regRepeatPassword: '',
+  regAddress: '',
+  regCity: '',
+  regCard: '',
+  regPhone: '',
 };
 
 export const RegisterReducer = (state = initialState, action) => {
@@ -10,6 +29,51 @@ export const RegisterReducer = (state = initialState, action) => {
       return {
         ...state,
         page: 'next',
+      };
+    case SET_REGISTER_PREVIOUS_PAGE:
+      return {
+        ...state,
+        page: 'previous',
+      };
+    case INPUT_REGISTER_FULL_NAME:
+      return {
+        ...state,
+        regFullName: action.payload,
+      };
+    case INPUT_REGISTER_EMAIL:
+      return {
+        ...state,
+        regEmail: action.payload,
+      };
+    case INPUT_REGISTER_PASSWORD:
+      return {
+        ...state,
+        regPassword: action.payload,
+      };
+    case INPUT_REGISTER_REPEAT_PASSWORD:
+      return {
+        ...state,
+        regRepeatPassword: action.payload,
+      };
+    case INPUT_REGISTER_ADDRESS:
+      return {
+        ...state,
+        regAddress: action.payload,
+      };
+    case INPUT_REGISTER_CITY:
+      return {
+        ...state,
+        regCity: action.payload,
+      };
+    case INPUT_REGISTER_CARD:
+      return {
+        ...state,
+        regCard: action.payload,
+      };
+    case INPUT_REGISTER_PHONE:
+      return {
+        ...state,
+        regPhone: action.payload,
       };
     default:
       return state;
