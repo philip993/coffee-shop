5f8a47ac027690af4f7277cf61ec332a3f9c8791
Filip Mihajlovski <fitz993m@gmail.com>
2021-03-31 14:35:14 +0200

    9.3. Style added again

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -5,7 +5,7 @@ const Cart = () => {
   return (
     <div className="cart">
       <div className="cartFirstDiv">
-        <h1>Cart</h1>
+        <h1>Shopping Cart</h1>
       </div>
     </div>
   );
