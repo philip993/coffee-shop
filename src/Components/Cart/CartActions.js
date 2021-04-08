1378e239a465b990e6fd855f253c45c1c9c4b00a
Filip Mihajlovski <fitz993m@gmail.com>
2021-04-08 15:34:19 +0200

    9.14. Actions for clear cart and remove one item

---
+++ src/Components/Cart/CartActions.js
@@ -0,0 +1,51 @@
+import {
+  SUCCESS_REMOVE_ITEM_FROM_CART,
+  FAILURE_REMOVE_ITEM_FROM_CART,
+  SUCCESS_CLEAR_CART,
+  FAILURE_CLEAR_CART,
+} from './CartActionTypes';
+import axios from 'axios';
+
+export const requestRemoveCartItem = (e) => {
+  return (dispatch, getState) => {
+    let currentUser;
+    let currentItem;
+
+    return axios
+      .delete(`http://localhost:3000/#/${currentUser}/${currentItem}`)
+      .then((response) => {
+        console.log(response);
+        dispatch({
+          type: SUCCESS_REMOVE_ITEM_FROM_CART,
+          payload: response.data.deletedItem,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch({
+          type: FAILURE_REMOVE_ITEM_FROM_CART,
+        });
+      });
+  };
+};
+
+export const requestClearCart = () => {
+  return (dispatch, getState) => {
+    let currentUser;
+    return axios
+      .delete(`http://localhost:3000/#/${currentUser}`)
+      .then((response) => {
+        console.log(response);
+        dispatch({
+          type: SUCCESS_CLEAR_CART,
+          payload: [],
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch({
+          type: FAILURE_CLEAR_CART,
+        });
+      });
+  };
+};
