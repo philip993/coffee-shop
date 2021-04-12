b16bb77f595f6880abf2e4b0ce7c4bec4e472630
Filip Mihajlovski <fitz993m@gmail.com>
2021-04-12 15:05:40 +0200

    9.17. Reducer finished

--- src/Components/Cart/CartReducer.js
+++ src/Components/Cart/CartReducer.js
@@ -1,4 +1,11 @@
-import { SUCCESS_CLEAR_CART, FAILURE_CLEAR_CART } from './CartActionTypes';
+import {
+  SUCCESS_CLEAR_CART,
+  FAILURE_CLEAR_CART,
+  SUCCESS_REMOVE_ITEM_FROM_CART,
+  FAILURE_REMOVE_ITEM_FROM_CART,
+  SUCCESS_GET_CART_ITEMS,
+  FAILURE_GET_CART_ITEMS,
+} from './CartActionTypes';
 
 const initialState = {
   cartItems: [],
@@ -11,10 +18,37 @@ export const CartReducer = (state = initialState, action) => {
       return {
         ...state,
         cartItems: action.payload,
+        cartError: false,
       };
     case FAILURE_CLEAR_CART:
       return {
         ...state,
+        cartError: true,
+      };
+    case SUCCESS_REMOVE_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartError: false,
+        cartItems: [
+          ...state.cartItems.slice(0, action.payload),
+          ...state.cartItems.slice(action.payload + 1),
+        ],
+      };
+    case FAILURE_REMOVE_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartError: true,
+      };
+    case SUCCESS_GET_CART_ITEMS:
+      return {
+        ...state,
+        cartError: false,
+        cartItems: action.payload,
+      };
+    case FAILURE_GET_CART_ITEMS:
+      return {
+        ...state,
+        cartError: true,
       };
     default:
       return state;
