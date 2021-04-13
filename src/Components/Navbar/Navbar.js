1213f61a4510c9897d66bb461696ca4e85966f44
Filip Mihajlovski <fitz993m@gmail.com>
2021-04-13 14:31:53 +0200

    9.20. Number of items in cart/tooltips for nav

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './NavbarStyle.scss';
 import { Link } from 'react-router-dom';
-import { Drawer, ListItem, Button, Badge } from '@material-ui/core';
+import { Drawer, ListItem, Button, Badge, Tooltip } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faShoppingCart,
   faUserCircle,
   faBars,
 } from '@fortawesome/free-solid-svg-icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { countItemsInCart } from '../Cart/CartActions';
 const Navbar = () => {
+  const { cartItemsNo } = useSelector((state) => ({
+    ...state.CartReducer,
+  }));
+  const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
   const handleOpen = () => {
     setOpen(true);
@@ -16,22 +22,36 @@ const Navbar = () => {
   const handleClose = () => {
     setOpen(false);
   };
+
+  console.log(cartItemsNo);
+
+  useEffect(() => {
+    dispatch(countItemsInCart());
+  }, []);
+
   return (
     <div className="navbarDiv">
       <div className="navbarFirstDiv">
-        <Button className="btn" onClick={handleOpen}>
-          <FontAwesomeIcon className="icon" icon={faBars} />
-        </Button>
+        <Tooltip title="Check Menu">
+          <Button className="btn" onClick={handleOpen}>
+            <FontAwesomeIcon className="icon" icon={faBars} />
+          </Button>
+        </Tooltip>
       </div>
       <div className="navbarSecondDiv">
-        <Button className="btn">
-          <FontAwesomeIcon className="icon" icon={faUserCircle} />
-        </Button>
-        <Button className="btn">
-          <Badge badgeContent={5} color="secondary">
-            <FontAwesomeIcon className="icon" icon={faShoppingCart} />
-          </Badge>
-        </Button>
+        <Tooltip title="Go to Profile">
+          <Button className="btn">
+            <FontAwesomeIcon className="icon" icon={faUserCircle} />
+          </Button>
+        </Tooltip>
+
+        <Tooltip title="Check Cart">
+          <Button className="btn">
+            <Badge badgeContent={5} color="secondary">
+              <FontAwesomeIcon className="icon" icon={faShoppingCart} />
+            </Badge>
+          </Button>
+        </Tooltip>
       </div>
 
       <Drawer id="drawer" open={open} onClose={handleClose}>
